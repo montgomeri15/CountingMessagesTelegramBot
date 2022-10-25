d4ead640536d9703448fe837819bef063cd72eb3
montgomeri15 <montgomeri15@gmail.com>
2022-10-25 14:33:07 +0300

    First commit: basic CountingMessagesBot

---
+++ src/main/java/org/example/MessageCalculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.logging.Logger;
+
+public class MessageCalculator extends TelegramLongPollingBot {
+    //public static final Logger log = LoggerFactory.getLogger();
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
